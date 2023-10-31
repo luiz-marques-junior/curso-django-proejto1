03241bdf061327bedecf485ad2cfe12d2153d83d
Luiz Marques Junior <junior.marques29@gmail.com>
2023-10-31 09:32:20 -0300

    Última atualização

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name':'Curso Django (Otávio Miranda)'
+    recipes = Recipe.objects.filter(is_plubished=True).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_plubished=True).order_by('-id')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
